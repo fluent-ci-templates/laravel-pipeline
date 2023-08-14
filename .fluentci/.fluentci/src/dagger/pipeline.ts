bb762b6d720116a9efe6de9b00fd62bdee1d0461
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-14 18:58:30 +0000

    ci: generate Github Actions workflow with `fluentci gh init`

--- .fluentci/.fluentci/src/dagger/pipeline.ts
+++ .fluentci/.fluentci/src/dagger/pipeline.ts
@@ -1,10 +1,27 @@
 import Client, { connect } from "@dagger.io/dagger";
-import { fmt, lint, test } from "./jobs.ts";
+import * as jobs from "./jobs.ts";
 
-export default function pipeline(src = ".") {
+const { fmt, lint, test, runnableJobs } = jobs;
+
+export default function pipeline(src = ".", args: string[] = []) {
   connect(async (client: Client) => {
+    if (args.length > 0) {
+      await runSpecificJobs(client, args as jobs.Job[]);
+      return;
+    }
+
     await fmt(client, src);
     await lint(client, src);
     await test(client, src);
   });
+}
+
+async function runSpecificJobs(client: Client, args: jobs.Job[]) {
+  for (const name of args) {
+    const job = runnableJobs[name];
+    if (!job) {
+      throw new Error(`Job ${name} not found`);
+    }
+    await job(client);
+  }
 }
