8b0dde6ee92034b60841ee22b7fcf6302f03add9
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-15 06:47:50 +0000

    make it compatible with Dagger Zenith

--- example/.fluentci/mod.ts
+++
@@ -1,1 +0,0 @@
-export * as Dagger from "./src/dagger/index.ts";
--- src/dagger/jobs.ts
+++ src/dagger/jobs.ts
@@ -1,4 +1,4 @@
-import Client, { withDevbox } from "../../deps.ts";
+import Client, { connect, withDevbox } from "../../deps.ts";
 
 export enum Job {
   test = "test",
@@ -12,64 +12,67 @@ export const exclude = [
   ".devbox",
 ];
 
-export const test = async (client: Client, src = ".") => {
-  const context = client.host().directory(src);
+export const test = async (src = "."): Promise<string> => {
+  await connect(async (client: Client) => {
+    const context = client.host().directory(src);
 
-  // get MariaDB base image
-  const mariadb = client
-    .container()
-    .from("mariadb:10.11.2")
-    .withEnvVariable("MARIADB_USER", Deno.env.get("MARIADB_USER") || "user")
-    .withEnvVariable(
-      "MARIADB_PASSWORD",
-      Deno.env.get("MARIADB_PASSWORD") || "password"
-    )
-    .withEnvVariable("MARIADB_DATABASE", "laravel")
-    .withEnvVariable(
-      "MARIADB_ROOT_PASSWORD",
-      Deno.env.get("MARIADB_ROOT_PASSWORD") || "root"
-    )
-    .withExposedPort(3306);
-
-  const baseCtr = withDevbox(
-    client
-      .pipeline(Job.test)
+    // get MariaDB base image
+    const mariadb = client
       .container()
-      .from("alpine:latest")
-      .withExec(["apk", "update"])
-      .withExec(["apk", "add", "bash", "curl"])
-      .withMountedCache("/nix", client.cacheVolume("nix"))
-      .withMountedCache("/etc/nix", client.cacheVolume("nix-etc"))
-  );
+      .from("mariadb:10.11.2")
+      .withEnvVariable("MARIADB_USER", Deno.env.get("MARIADB_USER") || "user")
+      .withEnvVariable(
+        "MARIADB_PASSWORD",
+        Deno.env.get("MARIADB_PASSWORD") || "password"
+      )
+      .withEnvVariable("MARIADB_DATABASE", "laravel")
+      .withEnvVariable(
+        "MARIADB_ROOT_PASSWORD",
+        Deno.env.get("MARIADB_ROOT_PASSWORD") || "root"
+      )
+      .withExposedPort(3306);
 
-  const ctr = baseCtr
-    .withMountedCache("/app/vendor", client.cacheVolume("composer-vendor"))
-    .withMountedCache(
-      "/app/node_modules",
-      client.cacheVolume("laravel-node_modules")
-    )
-    .withDirectory("/app", context, { exclude })
-    .withWorkdir("/app")
-    .withServiceBinding("db", mariadb)
-    .withExec(["cp", ".env.example", ".env"])
-    .withExec([
-      "sh",
-      "-c",
-      "devbox run -- composer install --no-interaction && \
-       devbox run -- npm install",
-    ])
-    .withExec(["sh", "-c", "devbox run -- php artisan key:generate"])
-    .withExec(["sh", "-c", "devbox run -- php artisan config:cache"])
-    .withExec(["sh", "-c", "devbox run -- php artisan migrate"])
-    .withExec(["sh", "-c", "devbox run -- php artisan db:seed"])
-    .withExec(["sh", "-c", "devbox run -- php vendor/bin/phpunit"]);
+    const baseCtr = withDevbox(
+      client
+        .pipeline(Job.test)
+        .container()
+        .from("alpine:latest")
+        .withExec(["apk", "update"])
+        .withExec(["apk", "add", "bash", "curl"])
+        .withMountedCache("/nix", client.cacheVolume("nix"))
+        .withMountedCache("/etc/nix", client.cacheVolume("nix-etc"))
+    );
 
-  const result = await ctr.stdout();
+    const ctr = baseCtr
+      .withMountedCache("/app/vendor", client.cacheVolume("composer-vendor"))
+      .withMountedCache(
+        "/app/node_modules",
+        client.cacheVolume("laravel-node_modules")
+      )
+      .withDirectory("/app", context, { exclude })
+      .withWorkdir("/app")
+      .withServiceBinding("db", mariadb)
+      .withExec(["cp", ".env.example", ".env"])
+      .withExec([
+        "sh",
+        "-c",
+        "devbox run -- composer install --no-interaction && \
+         devbox run -- npm install",
+      ])
+      .withExec(["sh", "-c", "devbox run -- php artisan key:generate"])
+      .withExec(["sh", "-c", "devbox run -- php artisan config:cache"])
+      .withExec(["sh", "-c", "devbox run -- php artisan migrate"])
+      .withExec(["sh", "-c", "devbox run -- php artisan db:seed"])
+      .withExec(["sh", "-c", "devbox run -- php vendor/bin/phpunit"]);
 
-  console.log(result);
+    const result = await ctr.stdout();
+
+    console.log(result);
+  });
+  return "done";
 };
 
-export type JobExec = (client: Client, src?: string) => Promise<void>;
+export type JobExec = (src?: string) => Promise<string>;
 
 export const runnableJobs: Record<Job, JobExec> = {
   [Job.test]: test,
